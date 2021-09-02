7818fdcb000f7c8a6a5c9f2377861df138f9871b
Alexander Salois <aslois.scholar@gmail.com>
2021-09-02 10:34:02 -0600

    changed var name

--- process.py
+++ process.py
@@ -4,9 +4,9 @@ mse.columns =["snr","MSE"]
 frmt = ".6f"
 for x in range(5,22):
     snr = mse[mse["snr"] == x]
-    snrtest = snr.iloc[-1,1]
+    snr_test = snr.iloc[-1,1]
     snr.drop(snr.tail(1).index)
     print("SNR =",x)
     print("CV mean MSE =",format(snr["MSE"].mean(),frmt))
-    print("Test MSE    =",format(snrtest,frmt))
+    print("Test MSE    =",format(snr_test,frmt))
     print()
