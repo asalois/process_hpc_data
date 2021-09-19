cb0c73914c41ec0111e04e1d67f1b103cda82c66
Alexander Salois <asalois.scholar@gmail.com>
2021-09-19 16:47:54 -0600

    switched to rsync

--- cpy_hyalite.sh
+++ cpy_hyalite.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-scp v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/pof_nns/fiber100/e10_12relu_12tanh_8tanh_4tanh_8tanh_12tanh/*.txt ./data
+rsync -P v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/lms_scan/*.mat ./data/lms
+rsync -P v16b915@hyalite.rci.montana.edu:/mnt/lustrefs/scratch/v16b915/dfe_scan/*.mat ./data/dfe
 
