2365c02aec7990b44a12092ef9216908fc74dbf1
Alexander Salois <asalois.scholar@gmail.com>
2021-09-20 11:18:47 -0600

    working on it

--- sort_lms.py
+++ sort_lms.py
@@ -34,8 +34,9 @@ cols.extend(snr)
 cols.append('full sum')
 df = pd.DataFrame(data1,columns=cols)
 df = df[df['full sum'] < 31]
-for k in range(5,26,5):
-    df['k end sum'] = df.iloc[:,-(k+1):-1].sum(axis=1)
+df['k end sum'] = df.iloc[:,-(5+1):-1].sum(axis=1)
+for k in range(5,31,5):
+    df['k end sum'] = df.iloc[:,-(k+1):-2].sum(axis=1)
     df = df.sort_values('k end sum')
     df1 = df.head(5)
     df_ber = df1.iloc[:,3:34].copy().T
