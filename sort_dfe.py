ea20e98a7c205ba796970d56884dca0aa5c448bf
Alexander Salois <asalois.scholar@gmail.com>
2021-09-19 18:18:31 -0600

    added more charts

--- sort_dfe.py
+++ sort_dfe.py
@@ -6,7 +6,6 @@ import time
 import numpy as np
 import pandas as pd
 start_time = time.time()
-k = 10
 
 for i in range(1,3543): # load in data
     matname = "data/dfe/" + str(i).zfill(4) + "_ber_eqs.mat"
@@ -19,8 +18,7 @@ for i in range(1,3543): # load in data
         data = [taps,ftaps,trainNum]
         data.extend(ber)
         data.append(sum(ber))
-        data.append(sum(ber[-k:]))
-        data_add = np.array(data).reshape(1,36)
+        data_add = np.array(data).reshape(1,35)
         data_add_ber = np.array(ber).reshape(31,1)
         if i == 1:
             data1 = data_add
@@ -34,22 +32,24 @@ cols = ['taps','ftaps','trainNum']
 snr = list(range(5,36))
 cols.extend(snr)
 cols.append('full sum')
-cols.append('k end sum')
 df = pd.DataFrame(data1,columns=cols)
-df = df.sort_values('k end sum')
-df = df.drop_duplicates(subset=['taps','ftaps','trainNum'])
-df1 = df.head(5)
-df_ber = df1.iloc[:,3:34].copy().T
-ber_cols = list()
-for index, row in df1.iterrows():
-    ber_cols.append(\
-            "tps={a:d} ftps={b:d} trn={c:d}"\
-            .format(a=int(row['taps']), b=int(row['ftaps']), c=int(math.log2(row['trainNum']))))
-df_ber.columns = ber_cols
-print(df)
-print(df1)
-print(df_ber)
-df_ber.plot(style='x-',kind='line',logy=False,\
-        title="LMS Scan Ber",xlabel="SNR dB",ylabel="BER")
-plt.show()
+for k in range(5,26,5):
+    df['k end sum'] = df.iloc[:,-(k+1):-1].sum(axis=1)
+    df = df.sort_values('k end sum')
+    df = df.drop_duplicates(subset=['taps','ftaps','trainNum'])
+    df1 = df.head(5)
+    df_ber = df1.iloc[:,3:34].copy().T
+    ber_cols = list()
+    for index, row in df1.iterrows():
+        ber_cols.append(\
+                "tps={a:d} ftps={b:d} trn={c:d}"\
+                .format(a=int(row['taps']), b=int(row['ftaps']), c=int(math.log2(row['trainNum']))))
+    df_ber.columns = ber_cols
+    #print(df)
+    print(df1)
+    print(df_ber)
+    df_ber.plot(style='x-',kind='line',logy=False,\
+            title="DFE Scan Ber",xlabel="SNR dB",ylabel="BER")
+    fig_name = "dfe_k_%d.png" % k
+    plt.savefig(fig_name)
 print("--- %.2f seconds ---" % (time.time() - start_time))
--- sort_lms.py
+++ sort_lms.py
@@ -6,7 +6,6 @@ import time
 import numpy as np
 import pandas as pd
 start_time = time.time()
-k = 10
 
 for i in range(1,2000): # load in data
     matname = "data/lms/" + str(i).zfill(3) + "_ber_eqs.mat"
@@ -19,8 +18,7 @@ for i in range(1,2000): # load in data
         data = [taps,step,trainNum]
         data.extend(ber)
         data.append(sum(ber))
-        data.append(sum(ber[-k:]))
-        data_add = np.array(data).reshape(1,36)
+        data_add = np.array(data).reshape(1,35)
         data_add_ber = np.array(ber).reshape(31,1)
         if i == 1:
             data1 = data_add
@@ -34,21 +32,24 @@ cols = ['taps','step','trainNum']
 snr = list(range(5,36))
 cols.extend(snr)
 cols.append('full sum')
-cols.append('k end sum')
 df = pd.DataFrame(data1,columns=cols)
-df = df.sort_values('k end sum')
-df1 = df.head(5)
-df_ber = df1.iloc[:,3:34].copy().T
-ber_cols = list()
-for index, row in df1.iterrows():
-    ber_cols.append(\
-            "tps={a:d} stp={b:1.3f} trn={c:d}"\
-            .format(a=int(row['taps']), b=row['step'], c=int(math.log2(row['trainNum']))))
-df_ber.columns = ber_cols
-#print(df)
-print(df1)
-print(df_ber)
-df_ber.plot(style='x-',kind='line',logy=True,\
-        title="LMS Scan Ber",xlabel="SNR dB",ylabel="BER")
-plt.show()
+for k in range(5,26,5):
+    df['k end sum'] = df.iloc[:,-(k+1):-1].sum(axis=1)
+    df = df.sort_values('k end sum')
+    df1 = df.head(5)
+    df_ber = df1.iloc[:,3:34].copy().T
+    ber_cols = list()
+    for index, row in df1.iterrows():
+        ber_cols.append(\
+                "tps={a:d} stp={b:1.3f} trn={c:d}"\
+                .format(a=int(row['taps']), b=row['step'], c=int(math.log2(row['trainNum']))))
+    df_ber.columns = ber_cols
+    #print(df)
+    print(df1)
+    print(df_ber)
+    df_ber.plot(style='x-',kind='line',logy=True,\
+            title="LMS Scan Ber",xlabel="SNR dB",ylabel="BER")
+    #plt.show()
+    fig_name = "lms_k_%d.png" % k
+    plt.savefig(fig_name)
 print("--- %.2f seconds ---" % (time.time() - start_time))
